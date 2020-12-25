ee593d63c6c525f7a20a4c77659c901498bebe99
sreenidhi3 <67953001+sreenidhi3@users.noreply.github.com>
2020-12-25 14:55:48 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+//variables
+var textInput = document.querySelector("#text-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var textOutput = document.querySelector("#text-output");
+var serverURL = "https://api.funtranslations.com/translate/shakespeare.json";
+
+//translate using funTranslations API
+function errorHandler(error) {
+    console.log("Error Occured :(",error);
+    alert("Server Error Occured");
+}
+
+function constructURL(text) {
+    return(serverURL+"?text="+text);
+}
+function fetchURL(text) {
+    fetch(constructURL(text)).then(response => response.json()).then(json => textOutput.innerText = json.contents.translated).catch(errorHandler);
+}
+
+function eventHandler() {
+    fetchURL(textInput.value);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
